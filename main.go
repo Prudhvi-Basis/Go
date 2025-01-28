ac325f0aaedd353da765a1cc5778ef00e4e5506f
Prudhvi <prudhviraj3525@gmail.com>
2025-01-28 07:51:32 +0530

    Go

--- main.go
+++ main.go
@@ -4,4 +4,10 @@ import "fmt"
 
 func main() {
     fmt.Println("Hello,Prudhvi")
+    var agency string = "Fast Track"
+    fmt.Println("Welcome to", agency) 
+    Trainer :="Raghu K"
+    fmt.Println("Hi,", Trainer, "!") 
+    var totalCars int = 50
+    fmt.Println(totalCars)
 }
